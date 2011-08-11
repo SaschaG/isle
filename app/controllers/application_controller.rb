bc8ebd49066e84316ed95283d7f1ce51813a69c0
Sascha G <sushi.genji@hotmail.de>
2011-08-11 16:06:56 +0200

    Redirect to new_village_path after registration works now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+    # Devise: Where to redirect users once they have logged in
+  def after_sign_in_path_for(resource)
+    new_village_path # <- Path you want to redirect the user to.
+  end
 end
